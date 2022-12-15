7c34d3e4571b23ddbd50dc9006efb28d8bcd459a
Lorrany <lolo.souza80@yahoo.com>
2022-12-15 15:58:30 -0300

    Corrige nome de função

--- distribuido/input.py
+++ distribuido/input.py
@@ -1,13 +1,11 @@
 import RPi.GPIO as GPIO
-
+import time
 
 def muda_estado_output(pino: int):
     print(pino)
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(pino,GPIO.OUT)
-    
-    if GPIO.output(pino) == GPIO.HIGH:
-        GPIO.output(pino,GPIO.LOW)
-    else:    
-        GPIO.output(pino,GPIO.HIGH)
+    GPIO.output(pino,GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(pino,GPIO.LOW)
--- distribuido/socket_distribuido.py
+++ distribuido/socket_distribuido.py
@@ -1,6 +1,6 @@
 import socket
 
-from input import muda_estado_input
+from input import muda_estado_output
 
 def socket_distribuido(_host: str,_port:int, data:any, data2:any):
     HOST = _host           # Endereco IP do Servidor
@@ -19,26 +19,26 @@ def socket_distribuido(_host: str,_port:int, data:any, data2:any):
             temp = msg.decode()
             if temp[0]=='1':
                 if temp[1]=='a':
-                    muda_estado_input(int(data["outputs"][0]["gpio"]))
+                    muda_estado_output(int(data["outputs"][0]["gpio"]))
                 elif temp[1]=='b':
-                    muda_estado_input(int(data["outputs"][1]["gpio"]))
+                    muda_estado_output(int(data["outputs"][1]["gpio"]))
                 elif temp[1]=='c':
-                    muda_estado_input(int(data["outputs"][3]["gpio"]))
+                    muda_estado_output(int(data["outputs"][3]["gpio"]))
                 elif temp[1]=='d':
-                    muda_estado_input(int(data["outputs"][2]["gpio"]))
+                    muda_estado_output(int(data["outputs"][2]["gpio"]))
                 elif temp[1]=='e':
-                    muda_estado_input(int(data["outputs"][4]["gpio"]))
+                    muda_estado_output(int(data["outputs"][4]["gpio"]))
             elif temp[0]=='2':
                 if temp[1]=='a':
-                    muda_estado_input(int(data2["outputs"][0]["gpio"]))
+                    muda_estado_output(int(data2["outputs"][0]["gpio"]))
                 elif temp[1]=='b':
-                    muda_estado_input(int(data2["outputs"][1]["gpio"]))
+                    muda_estado_output(int(data2["outputs"][1]["gpio"]))
                 elif temp[1]=='c':
-                    muda_estado_input(int(data2["outputs"][3]["gpio"]))
+                    muda_estado_output(int(data2["outputs"][3]["gpio"]))
                 elif temp[1]=='d':
-                    muda_estado_input(int(data2["outputs"][2]["gpio"]))
+                    muda_estado_output(int(data2["outputs"][2]["gpio"]))
                 elif temp[1]=='e':
-                    muda_estado_input(int(data2["outputs"][4]["gpio"]))
+                    muda_estado_output(int(data2["outputs"][4]["gpio"]))
         
         print ('Finalizando conexao do cliente', cliente)
         con.close()
